8b53b20dfab669007e6285a0bfc40e1d06fbc0b4
LTFYR <lutfiyariea@code.edu.az>
2022-02-10 08:55:04 +0400

    Add project files.

---
+++ 50denkicikdirse/Program.cs
@@ -0,0 +1,36 @@
+﻿
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+int[] arr = { };
+    if (num <= 50)
+{
+    for (int i = 4; i <= num; i++)
+    {
+        if (i % 3 == 0)
+        {
+            count++;
+        }
+    }
+}
+    else if (num > 50 && num <= 100)
+{
+    for (int i = 6; i < num; i++)
+    {
+        if (i % 5 == 0)
+        {
+            count++;
+            Console.WriteLine(i);
+        }
+    }
+}
+    else
+{
+    for (int i = 9; i <= num; i++)
+    {
+        if (i % 8 == 0)
+        {
+            count++;
+        }
+    }
+}
+Console.WriteLine(count);
